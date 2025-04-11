705ce71fa977bec7468b87f631357a58ff7e29fb
AlexTerkhov141 <terehovav52s@gmail.com>
2025-04-11 11:16:57 +0300

    refactor by requirements

--- lab2/src/details.js
+++ lab2/src/details.js
@@ -1,15 +1,18 @@
+const container = document.querySelector('.details');
 const urlParams = new URLSearchParams(window.location.search);
 const code = urlParams.get('code');
 
 fetch(`https://restcountries.com/v3.1/alpha/${code}`)
-  .then(res => res.json())
+  .then((res) => res.json())
   .then(([country]) => {
-    document.getElementById('details').innerHTML = `
+    const languages = Object.values(country.languages || {}).join(', ');
+
+    container.innerHTML = `
       <h2>${country.name.common}</h2>
       <p><strong>Capital:</strong> ${country.capital}</p>
       <p><strong>Region:</strong> ${country.region}</p>
       <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
-      <p><strong>Languages:</strong> ${Object.values(country.languages || {}).join(', ')}</p>
-      <img src="${country.flags.png}" width="200" />
+      <p><strong>Languages:</strong> ${languages}</p>
+      <img src="${country.flags.png}" width="200" alt="Flag of ${country.name.common}" />
     `;
   });
--- lab2/src/main.js
+++ lab2/src/main.js
@@ -1,8 +1,9 @@
-const container = document.getElementById('countries');
+const container = document.querySelector('.countries');
+
 fetch('https://restcountries.com/v3.1/all')
-  .then(res => res.json())
-  .then(data => {
-    data.forEach(country => {
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((country) => {
       const item = document.createElement('a');
       item.href = `details.html?code=${country.cca3}`;
       item.textContent = `${country.flag} ${country.name.common}`;
